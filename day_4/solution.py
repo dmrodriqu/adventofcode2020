7c38418f110daa883dd082cfc7cf5fb63312ca04
Dylan Rodriquez <dylanmr@uchicago.edu>
2020-12-05 01:57:30 -0500

    simplifying, nix exceptions var

--- day_4/solution.py
+++ day_4/solution.py
@@ -3,14 +3,13 @@ f = open('input.txt', 'r')
 
 texts = f.read()
 passports = texts.split('\n\n')
-categories = set(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'])
-exceptions = set(['cid'])
+categories = set(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'])
 invalid = 0
 for check in passports:
     data = check.replace('\n', ' ')
     info = dict(item.split(':') for item in data.split())
     diff = categories - set(info.keys())
-    if not(bool(diff) and bool(diff - exceptions)):
+    if not(bool(diff)):
         invalid+=1
 
 print(invalid)
